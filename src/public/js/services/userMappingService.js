61500bc42a12af8d9bd28fe96fcc00463deb35ec
Hekku2 <hekku2@gmail.com>
2015-11-22 17:28:49 +0200

    Added mapping for undefined role.

--- src/public/js/services/userMappingService.js
+++ src/public/js/services/userMappingService.js
@@ -3,8 +3,11 @@ var userServices = angular.module('userServices', []);
 userServices.factory('UserService', function() {
     var fac = {};
     fac.getRoleName = function(roleId){
-        var roles = ['', 'anon', 'user', 'admin'];
-        return roles[roleId];
+        var roles = [undefined, 'anon', 'user', 'admin'];
+        var name = roles[roleId];
+        if (!name)
+            return 'unknown';
+        return name;
     };
 
     return fac;
